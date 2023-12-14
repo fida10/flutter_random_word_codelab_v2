e44a620af8206ae2962ab6582ff984ce425f902b
eshad <fidaeshad10@gmail.com>
2023-12-14 03:07:47 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_random_word_codelab_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_random_word_codelab_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/test.dart
@@ -0,0 +1,3 @@
+void main(){
+  print("Added by eshad.");
+}
